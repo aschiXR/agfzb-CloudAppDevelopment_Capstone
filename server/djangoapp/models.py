78be72cfee5e5b446103764463e84d73cef7fbbd
aschiXR <aschi@artisect.com>
2021-05-06 19:45:28 +0000

    changes made from theia lab environment

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+from datetime  import datetime, time
 
 
 # Create your models here.
@@ -9,6 +10,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    Name = __name__
+    Description = 'description'
+
+    def __str__(self):
+        return CarMake
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +26,28 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    Name = __name__
+    DealerID = id
+    Type = ['Sedan', 'SUV', 'Wagon']
+    Year = datetime.date
+
+    def __str__(self):
+        return CarModel
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer(models.Model):
+    Name = __name__
+    DealerID = id
 
+    def __str__(self):
+        return CarDealer
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview(models.Model):
+    Rate = [1, 2, 3, 4, 5]
+    Comment = "Text"
+
+    def __str__(self):
+        return DealerReview
