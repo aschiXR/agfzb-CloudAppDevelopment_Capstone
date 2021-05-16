95451696046ff2304312bc36eac1fb3b15a15a49
aschi <aschi@artisect.com>
2021-05-16 12:28:52 +0200

    updated dealerships and dealer_details

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -76,31 +76,28 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
-    if request.method == 'GET':
-        url = '	https://1088d791-5891-4973-962a-8b6c0ad2b288-bluemix.cloudantnosqldb.appdomain.cloud/api/dealership'
-        dealerships = get_dealers_from_cf(url, **(request.GET))
-        context['dealerships'] = dealerships
+    if request.method == "GET":
+        url = ibm_cloud_url + ibm_cloud_api + get_dealerships_api
+        dealerships = get_dealers_from_cf(url)
+        dealership_list = []
+        for element in dealerships:
+            dealership_list.append(element)
+        context = {"dealership_list": dealership_list}
         return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
-def get_dealer_details(request, dealer_id):
-    context = {}
-    if request.method == 'GET':
-        url = '	https://1088d791-5891-4973-962a-8b6c0ad2b288-bluemix.cloudantnosqldb.appdomain.cloud/api/dealership'
-        dealerships = get_dealers_from_cf(url, **({'id':dealer_id}))
-        context['dealer'] = dealerships[0]
-        url = '	https://1088d791-5891-4973-962a-8b6c0ad2b288-bluemix.cloudantnosqldb.appdomain.cloud/api/review'
-        dealer_reviews = get_dealer_reviews_from_cf(url, dealer_id)
-        context['reviews'] = dealer_reviews
+def get_dealer_details(request, dealer_id, dealer_full_name):
+    if request.method == "GET":
+        url = ibm_cloud_url + ibm_cloud_api + get_reviews_api
+        reviews = get_dealer_review_from_cf(url, dealer_id)
+        review_list = []
+        for review in reviews:
+            review_list.append(review)
+        context = {"review_list": review_list, "dealer_id": dealer_id, "dealer_full_name": dealer_full_name}
         return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
 def add_review(request, dealer_id):
     context = {}
     if request.method == 'GET':
